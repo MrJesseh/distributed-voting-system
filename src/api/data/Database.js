d086e8ae20edac67d93503773488090b02ba6b2b
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2024-04-18 16:17:56 -0400

    Fix all the issues with ES Modules

--- src/api/data/Database.js
+++ src/api/data/Database.js
@@ -1,59 +1,58 @@
-const Datastore = require('@seald-io/nedb');
-const path = require('path');
-
+import Datastore from "@seald-io/nedb";
+import path from "node:path";
 
 /**
  Schemas
 
-{
-    id: number;
-    title: string;
-    options: string[];
-    results: number[];
-    isOpen: boolean;
+ {
+ id: number;
+ title: string;
+ options: string[];
+ results: number[];
+ isOpen: boolean;
+ }
+ */
+
+export class Database {
+  constructor() {
+    if (Database.instance) {
+      return Database.instance;
+    }
+
+    this.polls = new Datastore({ filename: path.join(import.meta.dirname, "./polls.db"), autoload: true });
+    Database.instance = this;
+  }
+
+  async pollExists(id) {
+    let data;
+    try {
+      data = await this.polls.findOneAsync({ id: id });
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    return data != null;
+  }
+
+  async createPoll(title, options, isOpen) {
+    let results = new Array(options.length).fill(0);
+    let id = new Date().getTime();
+    await this.polls.insertAsync({ id: id, title: title, options: options, results: results, isOpen: isOpen });
+  }
+
+  async incrementVote(id, option) {
+    // Ensure the poll that we are attempting to increment exists.
+    if (!this.pollExists(id)) {
+      return false;
+    }
+
+    // Create the update dynamically so we can specify an index.
+    const update = {};
+    update[`results.${option}`] = 1;
+
+    // Increment the specified vote.
+    await this.polls.updateAsync({ id: id }, { $inc: update }, { upsert: false });
+  }
 }
-*/
 
-class Database {
-    constructor(){
-        if(Database.instance){
-            return Database.instance;
-        }
-
-        this.polls = new Datastore({filename: path.join(__dirname, './polls.db'), autoload: true});
-        Database.instance = this;
-    }
-
-    async pollExists(id){
-        let data;
-        try{
-            data = await this.polls.findOneAsync({id: id});
-        }catch(error){
-            console.log(error);
-            return false;
-        }
-        if(data == null){
-            return false;
-        }
-        return true;
-    }
-
-    async createPoll(title, options, isOpen){
-        let results = new Array(options.length).fill(0);
-        let id = new Date().getTime();
-        await this.polls.insertAsync({id: id, title: title, options: options, results: results, isOpen: isOpen});
-    }
-    
-    async incrementVote(id, option){
-        // Ensure the poll that we are attempting to increment exists.
-        if(!this.pollExists(id)){return false;}
-
-        // Create the update dynamically so we can specify an index.
-        const update = {};
-        update[`results.${option}`] = 1;
-
-        // Increment the specified vote.
-        await this.polls.updateAsync({id: id}, { $inc: update }, {upsert: false});
-    }
-}
-module.exports = new Database();
+export let db = new Database();
--- src/api/queue.js
+++ src/api/queue.js
@@ -1,25 +1,37 @@
-const db = require('./data/Database');
-const Events = require('events');
+import { db } from "./data/Database.js";
+import Events from "events";
+
 const emitter = new Events();
 
-module.exports = class Queue{
-    constructor(){ this.queue = []; this.emitter = emitter;}
-    send(item){ this.queue.push(item); console.log(`${JSON.stringify(item)} added to queue.`);}
-    receive(){ return this.queue.shift(); }
-    startProcessing(interval) {
-        setInterval(async () => {
-            const item = this.receive();
-            if (item) {
-                console.log(`Processing Vote: ${JSON.stringify(item)}`);
+export class Queue {
+  constructor() {
+    this.queue = [];
+    this.emitter = emitter;
+  }
 
-                // Increment the vote in the database.
-                await db.incrementVote(item.id, item.option);
+  send(item) {
+    this.queue.push(item);
+    console.log(`${JSON.stringify(item)} added to queue.`);
+  }
 
-                // Send the votes through the event emitter showing that the queue has processed votes.
-                let votes = await db.polls.findOneAsync({id: item.id});
-                emitter.emit('processedQueue', votes);
-            }
-            
-        }, interval);
-    }
+  receive() {
+    return this.queue.shift();
+  }
+
+  startProcessing(interval) {
+    setInterval(async () => {
+      const item = this.receive();
+      if (item) {
+        console.log(`Processing Vote: ${JSON.stringify(item)}`);
+
+        // Increment the vote in the database.
+        await db.incrementVote(item.id, item.option);
+
+        // Send the votes through the event emitter showing that the queue has processed votes.
+        let votes = await db.polls.findOneAsync({ id: item.id });
+        emitter.emit("processedQueue", votes);
+      }
+
+    }, interval);
+  }
 }
